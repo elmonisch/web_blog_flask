1d8a31cee50b725809ce9289751d15725bb96b3d
elmonisch <hasifzulkifli95@gmail.com>
2020-08-23 23:06:31 +0800

    add

---
+++ src/app.py
@@ -0,0 +1,12 @@
+__author__ = 'ringbell'
+
+from flask import Flask 
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_method():
+    return "hello world"
+
+if __name__ == '__main__':
+    app.run(port=5000)
